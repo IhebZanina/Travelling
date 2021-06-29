e936a96a4b59215059c48e6e32a3a0d60ce03259
IhebZanina <iheb257@gmail.com>
2021-06-29 17:57:09 +0100

    make the list responsive (you must learn CSS)

--- src/Components/PoposList.js
+++ src/Components/PoposList.js
@@ -3,21 +3,34 @@ import PoposPlaces from "./PoposPlace";
 import "../CSS Components/PoposList.css";
 // this components will contain all the public places from the POPOSSpace one by one from one to n
 function PoposList() {
+  /* image dimensition should be: 1024*683*/
   return (
-    <div className="centered">
-      <div className="POPOSList">
-        <PoposPlaces
-          PlaceName="Parc national de Lomsdal-Visten"
-          PlaceImage="Parc national de Lomsdal-Visten.jpg"
-          PlaceDescription="interesting"
-        />
-
-        <PoposPlaces
-          PlaceName="Femundsmarka National Park"
-          PlaceImage="Femundsmarka National Park.jpg"
-          PlaceDescription="interesting"
-        />
-      </div>
+    <div className="POPOSList">
+      <PoposPlaces
+        PlaceName="Parc national de Lomsdal-Visten"
+        PlaceImage="Parc national de Lomsdal-Visten.jpg"
+        PlaceDescription="Interesting"
+      />
+      <PoposPlaces
+        PlaceName="Femundsmarka National Park"
+        PlaceImage="Femundsmarka National Park.jpg"
+        PlaceDescription="interesting"
+      />
+      <PoposPlaces
+        PlaceName="Parc national de Lomsdal-Visten"
+        PlaceImage="Parc national de Lomsdal-Visten.jpg"
+        PlaceDescription="Interesting"
+      />
+      <PoposPlaces
+        PlaceName="Parc national de Lomsdal-Visten"
+        PlaceImage="Parc national de Lomsdal-Visten.jpg"
+        PlaceDescription="Interesting"
+      />
+      <PoposPlaces
+        PlaceName="Parc national de Lomsdal-Visten"
+        PlaceImage="Parc national de Lomsdal-Visten.jpg"
+        PlaceDescription="Interesting"
+      />
     </div>
   );
 }
--- src/Components/PoposPlace.js
+++ src/Components/PoposPlace.js
@@ -2,20 +2,22 @@ import React from "react";
 import "../CSS Components/PoposPlace.css";
 
 function PoposPlace({ PlaceImage, PlaceName, PlaceDescription }) {
-  const splitterStyle = {
-    width: "34rem",
-  };
+  /*const splitterStyle = {
+    width: "25rem",
+  };*/
   return (
-    <div className="card" style={splitterStyle}>
-      <img
-        src={`${process.env.PUBLIC_URL}images/${PlaceImage}`}
-        height="315"
-        className="card-img-top"
-        alt={PlaceName}
-      />
-      <div className="card-body">
-        <h2 className="card-title">{PlaceName}</h2>
-        <p className="card-text">{PlaceDescription} </p>
+    <div className="responsive">
+      <div className="card">
+        <img
+          src={`${process.env.PUBLIC_URL}images/${PlaceImage}`}
+          height="auto"
+          className="card-img-top"
+          alt={PlaceName}
+        />
+        <div className="card-body">
+          <h2 className="card-title">{PlaceName}</h2>
+          <p className="card-text">{PlaceDescription} </p>
+        </div>
       </div>
     </div>
   );
